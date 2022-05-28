09a25536644f7c77de11199d66bad1dc13ef20b2
oubbaih <eoubbaih@gmail.com>
2022-05-28 23:12:31 +0100

    improve menu navigation

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let navbarTop = document.getElementById('navbar-id');
+console.log(navbarTop);
+window.addEventListener('scroll', (event) => {
+  let windowHeight = this.scrollY;
+  if (windowHeight > navbarTop.clientHeight) {
+    navbarTop.style =
+      'background-color:rgb(240, 240, 240); transition:background-color  ease-in-out 0.1s';
+  } else {
+    navbarTop.style = 'background-color:none';
+  }
+});

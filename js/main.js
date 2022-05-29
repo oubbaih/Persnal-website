aed512fb55c03b0b9fe54e57245e22ec04b84905
oubbaih <eoubbaih@gmail.com>
2022-05-30 00:15:34 +0100

    finish with home page

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ window.addEventListener('scroll', (event) => {
   let windowHeight = this.scrollY;
   if (windowHeight > navbarTop.clientHeight) {
     navbarTop.style =
-      'background-color:rgb(240, 240, 240); transition:background-color  ease-in-out 0.1s';
+      'background-color:rgba(15,20,30 , .2); transition:background-color  ease-in-out 0.1s';
   } else {
     navbarTop.style = 'background-color:transparent';
   }

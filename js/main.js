221b1876631ed594dce28d0935ad05afaaaffd94
oubbaih <eoubbaih@gmail.com>
2022-05-31 00:44:59 +0100

    hh

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
-let navbarTop = document.getElementById('navbar-id');
-console.log(navbarTop);
-window.addEventListener('scroll', (event) => {
-  let windowHeight = this.scrollY;
-  if (windowHeight > navbarTop.offsetTop) {
-    navbarTop.style =
-      'background-color:rgba(15,20,30 , .2); transition:background-color  ease-in-out 0.1s';
-  } else {
-    navbarTop.style = 'background-color:transparent';
-  }
-});
+// let navbarTop = document.getElementById('navbar-id');
+// console.log(navbarTop);
+// window.addEventListener('scroll', (event) => {
+//   let windowHeight = this.scrollY;
+//   if (windowHeight > navbarTop.offsetTop) {
+//     navbarTop.style =
+//       'background-color:rgba(15,20,30 , .2); transition:background-color  ease-in-out 0.1s';
+//   } else {
+//     navbarTop.style = 'background-color:transparent';
+//   }
+// });
 
 // Loading animation
 
